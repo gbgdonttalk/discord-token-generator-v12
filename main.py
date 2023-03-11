44e682c36e0ab6b9b40b6f2cd1e07bc8f7a6511e
gbgdonttalk <114838590+gbgdonttalk@users.noreply.github.com>
2023-03-11 15:27:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 
-print("https://github.com/gbgdonttalk/discord-token-generator-v12/releases/tag/V12.61 FREE VERSION WORKINGs")
+print("https://github.com/gbgdonttalk/discord-token-generator-v12/releases/tag/V12.61 FREE VERSION WORKING")
